0dd7195c28ed380d31a06ec7432c78e79ba6c182
SamindiYumethma <samindidahanayake@gmail.com>
2026-02-14 18:11:51 +0530

    tour packages update

--- tourism_app/src/app/tours/page.js
+++ tourism_app/src/app/tours/page.js
@@ -27,17 +27,18 @@ export default function Tours() {
       <TourCard 
         title="Package 01 (7 days)"
         description="Journey through Sri Lanka’s soul—wild safaris in Yala, misty hills of Ella, colonial charm in Nuwara Eliya, sacred Kandy, ancient caves in Dambulla, majestic Sigiriya, and gentle elephants in Pinnawala—ending with coastal serenity."
-        pdfUrl="\pdfs\tour1.pdf"
+        pdfUrl="\pdfs\one_updated.pdf"
       />
+      
       <TourCard 
-        title="Package 02 (3 days)"
-        description="Explore Sri Lanka’s wild safaris, misty hills, colonial charm, sacred temples, and elephant orphanage—an unforgettable journey from Yala to Kandy to the coast"
-        pdfUrl="\pdfs\tour2.pdf"
-      />
-      <TourCard 
-        title="Package 03 (4 days)"
+        title="Package 02 (4 days)"
         description="Discover Sri Lanka’s wild beauty and cultural charm in four days. From Yala’s thrilling safaris to Ella’s misty hills, colonial Nuwara Eliya, sacred Kandy, and gentle elephants in Pinnawala—this journey blends nature, heritage, and heartfelt hospitality."
-        pdfUrl="\pdfs\tour3.pdf"
+        pdfUrl="\pdfs\two_updated.pdf"
+      />
+      <TourCard 
+        title="Package 03 (3 days)"
+         description="Explore Sri Lanka’s wild safaris, misty hills, colonial charm, sacred temples, and elephant orphanage—an unforgettable journey from Yala to Kandy to the coast"
+        pdfUrl="\pdfs\three_updated.pdf"
       />
     </div>
   </div>
